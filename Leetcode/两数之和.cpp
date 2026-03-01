60fe47948a4f9f9394601ed796f193d0e25ad32e
boweilaster <boweilaster@gmail.com>
2026-03-01 20:45:43 +0800

    两数之和

--- Leetcode/两数之和.cpp
+++ Leetcode/两数之和.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 //暴力算法
@@ -16,4 +18,18 @@ public:
         }
         return {};
     }
+//哈希查找
+    vector<int>twosum_p(vector<int>& nums, int target) {
+       unordered_map<int, int> map; 
+        for(int i = 0; i < nums.size(); i++){
+            auto iter = map.find(target - nums[i]);
+            if(iter != map.end()){
+                return {iter->second, i};
+            }
+
+            map.insert(pair<int, int>(nums[i], i));
+        }
+        return {};
+    }
+    
 };
